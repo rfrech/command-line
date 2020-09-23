8bcb342056f768cd5c0d230a1ee7c0b74a42a766
rfrech <rfrechette@cnm.edu>
2020-09-23 08:47:52 -0700

    changed sum to long

--- src/edu/cnm/deepdive/Gauss.java
+++ src/edu/cnm/deepdive/Gauss.java
@@ -6,7 +6,7 @@ private static final int DEFAULT_UPPER_LIMIT = 100;
 
   public static void main (String[] args) {
     int upperLimit = (args.length >0) ? Integer.parseInt(args[0]) : DEFAULT_UPPER_LIMIT;
-    int sum = 0;
+    long sum = 0;
     for (int i = 1; i <= upperLimit; i++) {
       sum += i;
     }
